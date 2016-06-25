232c879efd37c9aaa8205924f3ec02198d5287b0
mgi166 <skskoari@gmail.com>
2016-06-25 22:45:15 +0900

    Updates routes order.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resource :user_sessions, only: [:new, :create, :destroy]
-
   get 'top/index'
-
+  resource :user_sessions, only: [:new, :create, :destroy]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
