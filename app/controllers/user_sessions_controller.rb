2a82c30c5ed655910e993201091ef386c7d23dd9
mgi166 <skskoari@gmail.com>
2016-06-25 23:55:06 +0900

    UserSessionController#create: Pass permitted parameter.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,8 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(user_session_params)
+
     if @user_session.save
       redirect_to users_path
     else
@@ -15,5 +16,11 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     redirect_to login_url
+  end
+
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:username, :password)
   end
 end
