2c485503699d91ec6ed7d0fac4279a82591764e9
mgi166 <skskoari@gmail.com>
2016-06-26 08:36:17 +0900

    Update spec description.

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe UserSessionsController, :type => :controller do
     end
   end
 
-  describe "GET create" do
+  describe "POST create" do
     it "returns http success" do
       post :create
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET destroy" do
+  describe "DELETE destroy" do
     it "returns http success" do
       delete :destroy
       expect(response).to have_http_status(:success)
