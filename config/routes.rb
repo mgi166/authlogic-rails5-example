5e6017c50df8c4784b9c9fa2e076f27a575afffc
mgi166 <skskoari@gmail.com>
2016-06-25 22:52:19 +0900

    Add alias routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get 'top/index'
   resource :user_sessions, only: [:new, :create, :destroy]
   resources :users
+
+  get '/signup', to: 'users#new'
+  get '/login', to: 'user_sessions#new'
+  delete '/logout', to: 'user_sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
