5705c28d302a943c4e47e372125d503b04875f8d
mgi166 <skskoari@gmail.com>
2016-06-25 23:50:49 +0900

    Add seed data for easily login.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.first_or_create!(
+  username: 'test',
+  email: 'test@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
